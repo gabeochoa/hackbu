3112c9b4ab033c3abd95e93c064ba7701c7d74fe
MatthewCh <mcheung1@binghamton.edu>
2014-04-26 14:24:19 -0700

    Old Code with New JS stuff

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,3 +1,7 @@
+var url_array = new Array("https://raw.githubusercontent.com/chrzhang/json_fodder/master/capitalsnew.json",
+                           "https://raw.githubusercontent.com/chrzhang/json_fodder/master/celebritiesnew.json",
+                           "https://raw.githubusercontent.com/chrzhang/json_fodder/master/baseballnew.json");
+var payload_value = 0;
 function HTTPGET(url) {
 	var req = new XMLHttpRequest();
 	req.open("GET", url, false);
@@ -7,14 +11,13 @@ function HTTPGET(url) {
 
 
 var getQuestion = function() {
-	//Get question info
-	var response = HTTPGET("https://raw.githubusercontent.com/chrzhang/json_fodder/master/jsontest.json");
+  var response = HTTPGET(url_array[payload_value]);
 		
 	//Convert to JSON
 	var json = JSON.parse(response);
 	
 	var questionname = json[4].name;
-    var questiondes = json[4].desc;
+  var questiondes = json[4].desc;
     
 	//Console output to check all is working.
 	//console.log("It is " + temperature + " degrees in " + location + " today!");
@@ -35,7 +38,7 @@ var getAnswer = function() {
 	var json = JSON.parse(response);
 	
 	var correctA = json[4].Answer1;
-    var wrongBCD = [json[4].Answer2, '/', json[4].Answer3, '/', json[4].Answer4];
+  var wrongBCD = [json[4].Answer2, '/', json[4].Answer3, '/', json[4].Answer4];
                    
 	//Construct a key-value dictionary	
 	var dict = {"KEY_CANSA" : correctA, "KEY_WANS": wrongBCD};
@@ -44,10 +47,14 @@ var getAnswer = function() {
 	Pebble.sendAppMessage(dict);
 };
 
-Pebble.addEventListener("ready",
-  function(e) {
-	//App is ready to receive JS messages
-	getQuestion();
-    getAnswer();
-  }
-);
+Pebble.addEventListener("appmessage",
+        function(e) {
+                console.log("digger");
+                 for(var key in e.payload){
+                         console.log(key);
+                         console.log(e.payload[key]);
+                          payload_value = e.payload[key];
+                          }
+                           getQuestion();
+                           getAnswer();
+  });
